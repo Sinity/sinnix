e086f2cbf2ab98dfb03dff67851f5da523b6740a
Sinity <sinity@users.noreply.github.com>
2024-09-24 17:39:42 +0200

    feat(hardware): enable bluetooth with fast-connect

--- modules/core/hardware.nix
+++ modules/core/hardware.nix
@@ -15,6 +15,23 @@
         edid-decode # for decoding EDID (display capabilities metadata, e.g. avaiable modes)
       ];
     };
+
+    bluetooth = {
+      enable = true;
+      settings = {
+        General = {
+	  Name = "Sinity-PC-BT";
+	  DiscoverableTimeout = 0;
+	  AlwaysPairable = true;
+	  PairableTimeout = 0;
+	  # FastConnectable = true;
+	};
+
+	Policy = {
+	  AutoEnable = true;
+	};
+      };
+    };
   };
   hardware.enableRedistributableFirmware = true;
 }
