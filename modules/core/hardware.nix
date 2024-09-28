b01b46c8e19cd08cd47d7202c8840243377a315c
Sinity <sinity@users.noreply.github.com>
2024-09-28 20:39:27 +0200

    feat(storage): add permanent storage mount points

--- modules/core/hardware.nix
+++ modules/core/hardware.nix
@@ -33,5 +33,35 @@
       };
     };
   };
+
+  # setup mounts of storage at boot
+  boot.supportedFilesystems = [ "btrfs" "ntfs" ];
+  fileSystems."/mnt/ssd_storage" = {
+    device = "/dev/disk/by-uuid/bd19092f-a195-47ab-9c0d-c923d1e5bfea";
+    fsType = "btrfs";
+    options = [
+      "strictatime" "lazytime"
+      "nofail"
+    ];
+  };
+  fileSystems."/mnt/hdd_storage" = {
+    device = "/dev/disk/by-uuid/5119B4113C747C42";
+    fsType = "ntfs";
+    options = [
+      "strictatime" "lazytime"
+      "nofail"
+      "uid=1000" "gid=100" "umask=000"
+      "big_writes"
+    ];
+  };
+  fileSystems."/mnt/arch_root" = {
+    device = "/dev/disk/by-uuid/481e214e-7bb6-49fa-bc87-ccb1f2c1e3c3";
+    fsType = "ext4";
+    options = [
+      "strictatime" "lazytime"
+      "nofail"
+    ];
+  };
+
   hardware.enableRedistributableFirmware = true;
 }
